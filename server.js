b096220ef607eda971793deb5b82d01b67ac0330
sev-valdez <severianovaldez@lithia.com>
2023-11-04 17:28:34 -0400

    Add initial google sign on configuration

--- server.js
+++ server.js
@@ -1,13 +1,39 @@
 const fs = require('fs');
 const path = require('path');
 const https = require('https');
+const helmet = require('helmet');
 const express = require('express');
+
+require('dotenv').config();
 
 const PORT = 3000;
 
+const config = {
+    CLIENT_ID: process.env.CLIENT_ID,
+    CLIENT_SECRET: process.env.CLIENT_SECRET,
+};
+
 const app = express();
 
-app.get('/secret', (req, res) => {
+app.use(helmet());
+
+function checkLoggedIn(req, res, next) {
+    const isLoggedIn = true; // TODO
+    if (!isLoggedIn) {
+        return res.status(401).json({
+            error: 'You must log in!'
+        });
+    }
+    next();
+};
+
+app.get('/auth/google', (req, res) => {});
+
+app.get('/auth/google/callback', (req, res) => {});
+
+app.get('/auth/logout', (req, res) => {});
+
+app.get('/secret', checkLoggedIn, (req, res) => {
     return res.send('Your personal secret value is 42!');
 });
 
